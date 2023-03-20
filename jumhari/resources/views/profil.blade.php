c91c74b77cd9b51fa3f703b9afe15df97428291f
jumhar1 <97357855+jumhar1@users.noreply.github.com>
2023-03-20 17:01:04 +0700

    Jumhari

---
+++ jumhari/resources/views/profil.blade.php
@@ -0,0 +1,2 @@
+<h1>Halaman Profil</h1>
+<h3>Hallo, {{$namaMahasiswa}}</h3>
--- jumhari/routes/web.php
+++ jumhari/routes/web.php
@@ -27,25 +27,29 @@ Route::get('/halo2', function () {
 });
 
 Route::get('/halo3', function () {
-    return'<h1>Hallo Semua, Selamat Siang</h1>';
+    return"<h1>Hallo Semua, Selamat Siang</h1>";
 });
 
  Route::get('/halo/(nama?)', function ($nama) {
-    echo'<h1>Hallo $nama</h1>';
+    echo"<h1>Hallo $nama</h1>";
  });
 
+ Route::get('/mahasiswa/{nama?}', function ($nama = 'Jumhari') {
+    echo "<h1>Halo $nama </h1>";
+});
+
  Route::get('/mahasiswa2/{nama?}/{jurusan?}', function ($nama = 'Jumhari', $jurusan = 'Informatika') {
-    echo '<h1>nama = $nama</h1>';
-    echo '<h1>jurusan = $jurusan</h1>';
+    echo "<h1>nama = $nama</h1>";
+    echo "<h1>jurusan = $jurusan</h1>";
 });
 
  Route::get('/mahasiswa3/{nama?}/{jurusan?}', function ($nama = 'Jumhari', $jurusan = 'Informatika') {
-    echo '<h1>nama = $nama</h1>';
-    echo '<h1>jurusan = $jurusan</h1>';
+    echo "<h1>nama = $nama</h1>";
+    echo "<h1>jurusan = $jurusan</h1>";
 })->where('nama','[A-Z]');
 
 Route::get('/hubungi' , function (){
-    echo '<h1>Hubungi Kami</h1>';
+    echo "<h1>Hubungi Kami</h1>";
 })->name('call');
 
 Route::get('/halo' , function (){
@@ -55,6 +59,34 @@ Route::get('/halo' , function (){
 Route::get('/halo2' , function (){
    return "<a href='".route('call')."'>contoh 2</a>"; 
 });
+
+
+
+//Route Group
+// Route::get('/mhs/nama' function() {
+//     return "<h2>nama</h2>";
+// });
+
+// Route::get('/mhs/jurusan' function() {
+//     return "<h2>jurusan</h2>"; 
+// });
+
+Route::prefix("/mhs")->group(function() {
+    Route::get("/nama", function() {
+        return "<h2>nama</h2>";
+    });
+    Route::get('/jurusan', function() {
+        return "<h2>jurusan</h2>";
+    });
+});
+
+//new //get, post,oatch, delete
+Route::get("/profil/{nama?}", function($nama = "Jumhari"){
+    return view("profil", ['namaMahasiswa' => $nama]); //optional 1
+});
+
+
+
 
 
 
