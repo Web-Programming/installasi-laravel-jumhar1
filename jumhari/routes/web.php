2faeccf32397df8b48fd5aed82a737aa567305eb
restianreza <jumhari_01@yahoo.co.id>
2023-03-20 15:14:59 +0700

    Jumhari

--- jumhari/routes/web.php
+++ jumhari/routes/web.php
@@ -16,3 +16,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//cara membuat Router
+Route::get('/halo', function () {
+    return'Hallo Semua';
+});
+
+Route::get('/halo2', function () {
+    return'<h1>Hallo Semua</h1>';
+});
+
+Route::get('/halo3', function () {
+    return'<h1>Hallo Semua, Selamat Siang</h1>';
+});
+
+ Route::get('/halo/(nama?)', function ($nama) {
+    echo'<h1>Hallo $nama</h1>';
+ });
+
+ Route::get('/mahasiswa2/{nama?}/{jurusan?}', function ($nama = 'Jumhari', $jurusan = 'Informatika') {
+    echo '<h1>nama = $nama</h1>';
+    echo '<h1>jurusan = $jurusan</h1>';
+});
+
+ Route::get('/mahasiswa3/{nama?}/{jurusan?}', function ($nama = 'Jumhari', $jurusan = 'Informatika') {
+    echo '<h1>nama = $nama</h1>';
+    echo '<h1>jurusan = $jurusan</h1>';
+})->where('nama','[A-Z]');
+
+Route::get('/hubungi' , function (){
+    echo '<h1>Hubungi Kami</h1>';
+})->name('call');
+
+Route::get('/halo' , function (){
+   return "<a href='".route('call')."'>'.route('call').'</a>"; 
+});
+
+Route::get('/halo2' , function (){
+   return "<a href='".route('call')."'>contoh 2</a>"; 
+});
+
+
+
